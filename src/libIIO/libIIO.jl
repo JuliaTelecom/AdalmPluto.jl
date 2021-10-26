11e0a3c7de47dad01faa936cb27189925a466793
Robin Gerzaguet <robin.gerzaguet@irisa.fr>
2021-10-26 17:40:31 +0200

    New udev rule ?

--- src/libIIO/libIIO.jl
+++ src/libIIO/libIIO.jl
@@ -69,13 +69,21 @@ function set_udev_rules()
     if Sys.islinux()
         if get_provider() == "default" || get_provider() == "yggdrasil"
             # if !isfile("/run/udev/rules.d/90-libiio.rules")
-                println("\nAdding the udev rules to /run/udev/rules.d/90-libiio.rules.\nDirectory is write protected, password prompt does not come from Julia");
-                rule = """SUBSYSTEM=="usb", PROGRAM=="/bin/sh -c '$libIIO_rootpath/bin/iio_info -S usb | grep -oE [[:alnum:]]{4}:[[:alnum:]]{4}'", RESULT!="", MODE="666"\n""";
-                open("/tmp/90-libiio.rules", "w") do f
-                    write(f, rule);
+            println("\nAdding the udev rules to /run/udev/rules.d/53-adi-plutosdr-usb.rules.\nDirectory is write protected, password prompt does not come from Julia");
+            rule = """SUBSYSTEM=="usb", ATTRS{idVendor}=="0456", ATTRS{idProduct}=="b674", MODE="0664", GROUP="plugdev"
+            SUBSYSTEM=="usb", ATTRS{idVendor}=="2fa2", ATTRS{idProduct}=="5a32", MODE="0664", GROUP="plugdev"
+            # SDR Device
+            SUBSYSTEM=="usb", ATTRS{idVendor}=="0456", ATTRS{idProduct}=="b673", MODE="0664", GROUP="plugdev"
+            SUBSYSTEM=="usb", ATTRS{idVendor}=="2fa2", ATTRS{idProduct}=="5a02", MODE="0664", GROUP="plugdev"
+            # tell the ModemManager (part of the NetworkManager suite) that the device is not a modem, 
+            # and don't send AT commands to it
+            SUBSYSTEM=="usb", ATTRS{idVendor}=="0456", ATTRS{idProduct}=="b673", ENV{ID_MM_DEVICE_IGNORE}="1"
+            SUBSYSTEM=="usb", ATTRS{idVendor}=="2fa2", ATTRS{idProduct}=="5a02", ENV{ID_MM_DEVICE_IGNORE}="1"\n""";
+            open("/tmp/53-adi-plutosdr-usb.rules", "w") do f
+                write(f, rule);
                 end
                 run(`sudo mkdir -p /run/udev/rules.d`);
-                run(`sudo cp /tmp/90-libiio.rules /run/udev/rules.d/90-libiio.rules`);
+                run(`sudo cp /tmp/53-adi-plutosdr-usb.rules /run/udev/rules.d/53-adi-plutosdr-usb.rules`);
             # else 
                 # println("Udev rules is already present in /run/rules.d/. Nothing to do")
             # end
